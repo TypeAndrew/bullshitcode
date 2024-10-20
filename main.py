d091634eb6d72253ea4ef8a1bb9df491028505be
Andrew <69585440+TypeAndrew@users.noreply.github.com>
2024-10-20 21:46:58 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,1 @@
+print("Високосний" if (lambda y: str(y / 4)[len(str(y / 4))-2:] == '.0' and (str(y / 100)[len(str(y/ 4))-2:] != '.0' or str(y / 400)[len(str(y/ 4))-2:] == '.0'))(int(input("Введіть рік: "))) else "Не високосний")
